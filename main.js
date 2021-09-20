2ef96a4fec897f7aed6743b84d97fdbb405091b7
Nilo Rodriguez <niloeleuterio@gmail.com>
2021-09-19 21:43:21 -0300

    41 minutos da aula 3 - Sombra corrigida no HEADER

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-/* abre e fecha o menu quando clicar no icone: hamburguer e x */
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
-const toogle = document.querySelectorAll('nav .toggle')
+const toggle = document.querySelectorAll('nav .toggle')
 
-for (const element of toogle) {
+for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 
-/* qnd clicar em um item do menu, esconder o menu */
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -17,17 +17,18 @@ for (const link of links) {
   })
 }
 
-/*mudar o header da página quando der scroll*/
+/* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
-console.log(navHeight)
 
 window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
-    //scroll é maior que a altura do header
+    // scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
+    // menor que a altura do header
     header.classList.remove('scroll')
-    //menor que a altura do header
   }
 })
+
+/* Testimonials carousel slider swaper */
